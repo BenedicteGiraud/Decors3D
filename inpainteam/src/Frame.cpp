4fbae3aa695e705453ecd5d3cd380806ff7ba4c8
Thomas Rebele <thomas.rebele@googlemail.com>
2014-05-28 20:47:21 +0200

    annotated opencv feature detector call

--- inpainteam/src/Frame.cpp
+++ inpainteam/src/Frame.cpp
@@ -8,7 +8,11 @@
 #include "Frame.h"
 
 using namespace cv;
+#include <cv.h>
+#include <highgui.h>
+
 #include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/features2d/features2d.hpp>
 
 Frame::Frame(Mat image) {
 	this->image = image;
@@ -20,8 +24,22 @@ Frame::~Frame() {
 
 void Frame::detectKeyPoints() {
 
-	int minHessian;
-	SurfFeatureDetector detector(minHessian);
+	SurfFeatureDetector detector(
+			100, // hessianThreshold
+			1, // nOctaves
+			4, // nOctaveLayers
+			true, // extended
+			false // upright
+			);
+
+	/*GFTTDetector detector(
+			10000, // maxCorners
+			0.02, // qualityLevel
+			4, // minDistance
+			5, // blockSize
+			true, // useHarrisDetector
+			0.04); // k*/
+
 
 	detector.detect(image, keypoints);
 }
