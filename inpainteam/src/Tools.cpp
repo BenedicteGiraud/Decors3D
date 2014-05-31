ab4744e63b6c3f28808f3c6b5dafe49387b6bf96
Thomas Rebele <thomas.rebele@googlemail.com>
2014-05-31 11:00:32 +0200

    adding Tools class

---
+++ inpainteam/src/Tools.cpp
@@ -0,0 +1,18 @@
+/*
+ * Tools.cpp
+ *
+ *  Created on: 2014/05/31
+ *      Author: tr
+ */
+
+#include "Tools.h"
+
+Tools::Tools() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Tools::~Tools() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ inpainteam/src/Tools.h
@@ -0,0 +1,31 @@
+/*
+ * Tools.h
+ *
+ *  Created on: 2014/05/31
+ *      Author: tr
+ */
+
+#ifndef TOOLS_H_
+#define TOOLS_H_
+
+using namespace cv;
+
+class Tools {
+public:
+	Tools();
+	virtual ~Tools();
+
+	inline static void verticalConcatenateMatrices(cv::Mat input, cv::Mat add, cv::Mat output) {
+		if(input.rows == 0) {
+			output = add;
+		}
+		else if(add.rows == 0) {
+			output = input;
+		}
+		else {
+			vconcat(input, add, output);
+		}
+	}
+};
+
+#endif /* TOOLS_H_ */
